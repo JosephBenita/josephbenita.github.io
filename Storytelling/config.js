231e98878142d3a1c4f46142577bc2af7f359674
JosephBenita <joseph@benita.cc>
2022-09-24 18:46:12 +0000

    Update config.js

--- Storytelling/config.js
+++ Storytelling/config.js
@@ -12,7 +12,7 @@ var config = {
             id: 'intro1',
             alignment: 'right',
             title: 'Africa desk presentation',
-            image: 'images/form_logo.png',
+            image: 'storytelling/images/form_logo.png',
             description: 'intro',
             location: {
                 center: [39, 13],
@@ -25,7 +25,7 @@ var config = {
             id: 'intro2',
             alignment: 'right',
             title: 'Global strategy',
-            image: 'images/image1.png',
+            image: 'storytelling/images/image1.png',
             description: 'intro',
             location: {
                 center: [39, 13],
